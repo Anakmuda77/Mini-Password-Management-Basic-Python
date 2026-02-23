fcf44f1dcb2a72ffbf0f1869b9972b2d545829fd
Anakmuda77 <johnsonliu747@gmail.com>
2026-02-23 09:18:51 +0800

    memperbaikit fitur add akun dan menambahkan fitur melihat list akun

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from userFunc import user
 # typePaswd = input("Masukkan Jenis Password yang ingin anda masukkan")
 # userName = input("Masukkan Passowrd Anda")
 
-typePaswd = "Instagram"
-userName = "Dito"
+typePaswd = "dfdfdfdfdfdfdfdf"
+userName = "preman"
 password = "Macanpekutuk"
 
 NewUser = user.user(typePaswd,userName,password)
-NewUser.createAccount()
+# NewUser.createAccount()
+NewUser.seeAllAccount()
+
+
--- userFunc/user.py
+++ userFunc/user.py
@@ -1,17 +1,41 @@
 import json
-paswdJson = dict()
 path = r'C:\Users\advan\Documents\Pemrograman\Python\Basic Exercise\Mini Password Manager/passwords.json'
 
 
 class user:
+    # Constructor method
     def __init__(self,account,name,password):
         self.account = account
         self.name = name
         self.password = password
 
+    # Method untuk membuat Akun baru
     def createAccount(self):
-        paswdJson[self.account] = {'username':self.name, 'password': self.password}
-        with open(path,'a') as f:
-            f.write(json.dumps(paswdJson))
+        paswdJson = dict()
+        # Mengecek apakah sudah terdapat file dengan nama tersebut atau tidak
+        try:
+            with open(path,'r') as f:
+                # Mengubah Json jadi dictionary
+                paswdJson = json.loads(f.read())
+                paswdJson[self.account] ={'username':self.name, 'password': self.password}
 
+                with open(path,'w') as f :
+                    f.write(json.dumps(paswdJson))
+
+        
+        except:
+            paswdJson[self.account] = {'username':self.name, 'password': self.password}
+            with open(path,'a') as f:
+                f.write(json.dumps(paswdJson))
+
+
+    # Melihat list Akun yang sudah disimpan
+    def seeAllAccount(self):
+        with open (path,'r') as f :
+            paswdJson = json.loads(f.read())
+            print("-----Koleksi Akun----")
+            for i in list(paswdJson):
+                print("Tipe Akun : ",i)
+                print("- Username :  ",paswdJson[i]['username'])
+                print("- Password :  ",paswdJson[i]['password'])
 
